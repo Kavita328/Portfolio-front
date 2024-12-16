70cd162ea0515a0a6a890169bb26534b934d3ad1
Kavita <bkavita1998@gmail.com>
2024-12-16 22:05:18 +0100

    fetch url change

--- Prestations/script.js
+++ Prestations/script.js
@@ -1,26 +1,26 @@
 
-  // fetch(`https://portfolio-back-delta.vercel.app/videos`)
-  // .then((response) => response.json())
-  // .then((data) => {
-  //  console.log('front:', data)
+  fetch(`https://portfolio-back-coral.vercel.app/videos`)
+  .then((response) => response.json())
+  .then((data) => {
+   console.log('front:', data)
    
-  //           data.videos.items.forEach(video => {
-  //             const videoId = video.id.videoId;
-  //             console.log('video', videoId)
-  //             const title = video.snippet.title;
-  //             const thumbnail = video.snippet.thumbnails.high.url;
+            data.videos.items.forEach(video => {
+              const videoId = video.id.videoId;
+              console.log('video', videoId)
+              const title = video.snippet.title;
+              const thumbnail = video.snippet.thumbnails.high.url;
       
             
-  //             document.getElementById('video-list').innerHTML += `
-  //               <div class="video">
-  //                 <a href="https://www.youtube.com/watch?v=${videoId}" target="_blank">
-  //                   <img src="${thumbnail}" alt="${title}" />
-  //                   <h3>${title}</h3>
-  //                 </a>
-  //               </div>
-  //             `;
+              document.getElementById('video-list').innerHTML += `
+                <div class="video">
+                  <a href="https://www.youtube.com/watch?v=${videoId}" target="_blank">
+                    <img src="${thumbnail}" alt="${title}" />
+                    <h3>${title}</h3>
+                  </a>
+                </div>
+              `;
        
-  // })})
+  })})
 
 
   
